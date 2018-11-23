ec994adc02c2ff8253f91c15f749df088fe2b49c
DMorrison <declan@morrison-okeefe.com>
2018-11-23 03:45:41 -0500

    working build

---
+++ clicky-game/src/assets/images.js
@@ -0,0 +1,64 @@
+const images = [
+  {
+    clicked: false,
+    src: 'http://icons.iconarchive.com/icons/jonathan-rey/simpsons/256/Homer-Simpson-04-Happy-icon.png',
+    alt: 'Homer Simpson'
+  },
+  {
+    clicked: false,
+    src: 'http://aux2.iconspalace.com/uploads/marge-simpson-icon-256.png',
+    alt: 'Marge Simpson'
+  },
+  {
+    clicked: false,
+    src: 'https://vignette.wikia.nocookie.net/simpsons/images/e/ed/Lisa-Simpson-icon-1.png/revision/latest?cb=20170628205647',
+    alt: 'Lisa Simpson'
+  },
+  {
+    clicked: false,
+    src: 'http://aux.iconspalace.com/uploads/bart-simpson-icon-256.png',
+    alt: 'Bart Simpson'
+  },
+  {
+    clicked: false,
+    src: 'https://vignette.wikia.nocookie.net/simpsons/images/d/df/Maggie-Simpson-icon.png/revision/latest?cb=20140817104831',
+    alt: 'Maggie Simpson'
+  },
+  {
+    clicked: false,
+    src: 'https://pbs.twimg.com/profile_images/2876039783/3e8e5c214eeedf709dd0c6cbf8273d28.png',
+    alt: 'Ned Flanders'
+  },
+  {
+    clicked: false,
+    src: 'https://pbs.twimg.com/profile_images/728176541604130816/m5Xm5VuU_400x400.jpg',
+    alt: 'Barney Gumble'
+  },
+  {
+    clicked: false,
+    src: 'https://pbs.twimg.com/profile_images/378800000083233222/bae865fed093884c9629c01c1d57078c.png',
+    alt: 'Moe Szyslak'
+  },
+  {
+    clicked: false,
+    src: 'https://pbs.twimg.com/profile_images/741693532780548096/5dVFXFGw_400x400.jpg',
+    alt: 'Carl Carlson'
+  },
+  {
+    clicked: false,
+    src: 'https://d304k3mn1nwj0a.cloudfront.net/the_simpsons/chiefwiggum.png',
+    alt: 'Chief Wiggum'
+  },
+  {
+    clicked: false,
+    src: 'https://pbs.twimg.com/profile_images/3391909880/b67575645683482013222b22f081f34e.png',
+    alt: 'Otto Mann'
+  },
+  {
+    clicked: false,
+    src: 'https://pbs.twimg.com/profile_images/704156139081039872/peAF3o8U_400x400.jpg',
+    alt: 'Mr. Burns'
+  },
+]
+
+export default images;
