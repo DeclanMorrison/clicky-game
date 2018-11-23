b5d0e803f86c909755d22d0582ee6849f93f71e7
DMorrison <declan@morrison-okeefe.com>
2018-11-23 03:55:15 -0500

    Updates

---
+++ precache-manifest.2e8e72e55a4f3397eb9731447498d876.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "63952800ae5add75ab21",
+    "url": "/clicky-game/static/js/main.63952800.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/clicky-game/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "cf5ff55cea37d859ce5f598a792ca752",
+    "url": "/clicky-game/index.html"
+  }
+];
